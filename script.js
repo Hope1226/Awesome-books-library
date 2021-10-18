c3818351a0cff84e7064637507228216b09e986f
AlexRS90 <aramosbengy@gmail.com>
2021-10-18 16:58:58 -0500

    Start delete function

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
+//const { count } = require("console");
+
 const form = document.querySelector('form');
 const addBookBtn = document.querySelector('#addBookBtn');
 const listOfBooks = document.querySelector('.books-list');
 const bookTitle = document.querySelector('#bookTitle');
 const bookAuthor = document.querySelector('#bookAuthor');
+const deleteBtn = document.querySelectorAll('.removeBookBtn');
 
 const library = (()=> {
   const listOfBook = [];
   const addBook = (title, author) => {
     listOfBook.push({title: `${title}`, author: `${author}`});
   }
+  const removeBook = index => {
+    listOfBook.splice(index, 1);
+  }
 
-  return {listOfBook, addBook}
+  return {listOfBook, addBook, removeBook}
 })();
 
 form.addEventListener('submit', (event)=>{
@@ -20,10 +26,17 @@ form.addEventListener('submit', (event)=>{
   <div class="book">
     <h2 class="bookTitle">${library.listOfBook[library.listOfBook.length - 1].title}</h2>
     <p class="bookAuthor">${library.listOfBook[library.listOfBook.length - 1].author}</p>
-    <button type="button" id="removeBookBtn">Remove</button>
+    <button type="button" onclick="library.removeBook(0)" class="removeBookBtn">Remove</button>
   </div>`
   form.reset();
+  
 });
+
+// deleteBtn.addEventListener('click', (event) => {
+//   library.removeBook(0);
+// });
+
+
 
 
 
