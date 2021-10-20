535b4a8536c688264ec1b3fa40004e2ca6bb9859
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2021-10-20 20:47:16 +0500

    Make all methods functional via class

--- script.js
+++ script.js
@@ -5,28 +5,28 @@ const bookAuthor = document.querySelector('#bookAuthor');
 const localBooks = localStorage.getItem('localLibrary');
 
 class Library {
-  constructor (name) {
+  constructor (name, listOfBook) {
     this.name = name;
-     this.listOfBook;
+    this.listOfBook = listOfBook;
   }
   
   setLocalInfo () {
     if (localBooks === null) {
-      listOfBook = [];
+      this.listOfBook = [];
     } else {
-      listOfBook = JSON.parse(localBooks);
-      for (let i = 0; i < listOfBook.length; i += 1) {
+      this.listOfBook = JSON.parse(localBooks);
+      for (let i = 0; i < this.listOfBook.length; i += 1) {
         listOfBooks.innerHTML += `
         <div class="book"}>  
-        <h2 class="bookTitle">${listOfBook[i].title}</h2>
-        <p class="bookAuthor">${listOfBook[i].author}</p>
-        <button type="button" onclick="this.parentNode.parentNode.removeChild(this.parentNode)" value="${listOfBook[i].title}" class="removeBookBtn">Remove</button>
+        <h2 class="bookTitle">${this.listOfBook[i].title}</h2>
+        <p class="bookAuthor">${this.listOfBook[i].author}</p>
+        <button type="button" onclick="this.parentNode.parentNode.removeChild(this.parentNode)" value="${this.listOfBook[i].title}" class="removeBookBtn">Remove</button>
         </div>`;
 
         const removeBookBtns = document.querySelectorAll('.removeBookBtn');
         removeBookBtns.forEach((button) => {
           button.addEventListener('click', () => {
-            library.removeBook(button.value);
+          this.removeBook(button.value);
           });
         });
       }
@@ -35,12 +35,12 @@ class Library {
 
   addBook = (title, author) => {
     this.listOfBook.push({ title: `${title}`, author: `${author}` });
-    window.localStorage.setItem('localLibrary', JSON.stringify(listOfBook));
+    window.localStorage.setItem('localLibrary', JSON.stringify(this.listOfBook));
   };
 
   updateStorage = () => {
     localStorage.clear();
-    localStorage.setItem('localLibrary', JSON.stringify(listOfBook));
+    localStorage.setItem('localLibrary', JSON.stringify(this.listOfBook));
   };
 
   removeBook = (keyWord) => {
@@ -48,22 +48,26 @@ class Library {
     this.listOfBook.splice(index, 1);
     this.updateStorage();
   };
-}
+};
+
+const lib1 = new Library('testLibrary', []);
+lib1.setLocalInfo();
+
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  library.addBook(bookTitle.value, bookAuthor.value);
+  lib1.addBook(bookTitle.value, bookAuthor.value);
   listOfBooks.innerHTML += `
   <div class="book"}>
-    <h2 class="bookTitle">${library.listOfBook[library.listOfBook.length - 1].title}</h2>
-    <p class="bookAuthor">${library.listOfBook[library.listOfBook.length - 1].author}</p>
-    <button type="button" onclick="this.parentNode.parentNode.removeChild(this.parentNode)" value="${library.listOfBook[library.listOfBook.length - 1].title}" class="removeBookBtn">Remove</button>
+    <h2 class="bookTitle">${lib1.listOfBook[lib1.listOfBook.length - 1].title}</h2>
+    <p class="bookAuthor">${lib1.listOfBook[lib1.listOfBook.length - 1].author}</p>
+    <button type="button" onclick="this.parentNode.parentNode.removeChild(this.parentNode)" value="${lib1.listOfBook[lib1.listOfBook.length - 1].title}" class="removeBookBtn">Remove</button>
   </div>`;
 
   const removeBookBtns = document.querySelectorAll('.removeBookBtn');
   removeBookBtns.forEach((button) => {
     button.addEventListener('click', () => {
-      library.removeBook(button.value);
+      lib1.removeBook(button.value);
     });
   });
   form.reset();
