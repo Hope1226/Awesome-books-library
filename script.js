a3a3ad11b5c2f99dda1a23566b2d68f64782bb0a
AlexRS90 <aramosbengy@gmail.com>
2021-10-20 10:17:50 -0500

    Switch modals per Class

--- script.js
+++ script.js
@@ -4,46 +4,51 @@ const bookTitle = document.querySelector('#bookTitle');
 const bookAuthor = document.querySelector('#bookAuthor');
 const localBooks = localStorage.getItem('localLibrary');
 
-const library = (() => {
-  let listOfBook;
-  if (localBooks === null) {
-    listOfBook = [];
-  } else {
-    listOfBook = JSON.parse(localBooks);
-    for (let i = 0; i < listOfBook.length; i += 1) {
-      listOfBooks.innerHTML += `
-      <div class="book"}>  
-      <h2 class="bookTitle">${listOfBook[i].title}</h2>
-      <p class="bookAuthor">${listOfBook[i].author}</p>
-      <button type="button" onclick="this.parentNode.parentNode.removeChild(this.parentNode)" value="${listOfBook[i].title}" class="removeBookBtn">Remove</button>
-      </div>`;
+class Library {
+  constructor (name) {
+    this.name = name;
+     this.listOfBook;
+  }
+  
+  setLocalInfo () {
+    if (localBooks === null) {
+      listOfBook = [];
+    } else {
+      listOfBook = JSON.parse(localBooks);
+      for (let i = 0; i < listOfBook.length; i += 1) {
+        listOfBooks.innerHTML += `
+        <div class="book"}>  
+        <h2 class="bookTitle">${listOfBook[i].title}</h2>
+        <p class="bookAuthor">${listOfBook[i].author}</p>
+        <button type="button" onclick="this.parentNode.parentNode.removeChild(this.parentNode)" value="${listOfBook[i].title}" class="removeBookBtn">Remove</button>
+        </div>`;
 
-      const removeBookBtns = document.querySelectorAll('.removeBookBtn');
-      removeBookBtns.forEach((button) => {
-        button.addEventListener('click', () => {
-          library.removeBook(button.value);
+        const removeBookBtns = document.querySelectorAll('.removeBookBtn');
+        removeBookBtns.forEach((button) => {
+          button.addEventListener('click', () => {
+            library.removeBook(button.value);
+          });
         });
-      });
+      }
     }
   }
-  const addBook = (title, author) => {
-    listOfBook.push({ title: `${title}`, author: `${author}` });
+
+  addBook = (title, author) => {
+    this.listOfBook.push({ title: `${title}`, author: `${author}` });
     window.localStorage.setItem('localLibrary', JSON.stringify(listOfBook));
   };
 
-  const updateStorage = () => {
+  updateStorage = () => {
     localStorage.clear();
     localStorage.setItem('localLibrary', JSON.stringify(listOfBook));
   };
 
-  const removeBook = (keyWord) => {
-    const index = listOfBook.findIndex((x) => x.title === keyWord);
-    listOfBook.splice(index, 1);
-    updateStorage();
+  removeBook = (keyWord) => {
+    const index = this.listOfBook.findIndex((x) => x.title === keyWord);
+    this.listOfBook.splice(index, 1);
+    this.updateStorage();
   };
-
-  return { listOfBook, addBook, removeBook };
-})();
+}
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
